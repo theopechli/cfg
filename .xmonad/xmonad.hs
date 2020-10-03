ed73de8794aa2680d403859958a37f648899b07c
Theofilos Pechlivanis <49034471+theopechli@users.noreply.github.com>
2020-10-03 19:45:22 +0300

    Update config and set xcursor

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -9,11 +9,12 @@ import XMonad.Layout.Gaps
 
 main = do
     xmproc <- spawnPipe "xmobar"
+    cproc <- spawnPipe "xsetroot -cursor_name left_ptr"
     rsproc <- spawnPipe "redshift"
     fproc <- spawnPipe "feh --randomize --bg-fill ~/Pictures/*"
     xmonad $ docks def
         { modMask = mod4Mask
-        , terminal = "st"
+        , terminal = "urxvt"
         , layoutHook = avoidStruts $ layoutHook def
         , logHook = dynamicLogWithPP $ def
         { ppOutput = hPutStrLn xmproc
@@ -24,14 +25,20 @@ main = do
         [ ((mod4Mask, xK_r), spawn "dmenu_run")
         , ((0, xF86XK_MonBrightnessUp), spawn "light -A 10")
         , ((0, xF86XK_MonBrightnessDown), spawn "light -U 10")
-        , ((0, xF86XK_AudioMute), spawn "amixer -q set Master toggle")
-        , ((0, xF86XK_AudioLowerVolume), spawn "amixer -q set Master 1%-")
-        , ((0, xF86XK_AudioRaiseVolume), spawn "amixer -q set Master 1%+")
+--        , ((0, xF86XK_AudioMute), spawn "amixer -q set Master toggle")
+--        , ((0, xF86XK_AudioLowerVolume), spawn "amixer -q set Master 1%-")
+--        , ((0, xF86XK_AudioRaiseVolume), spawn "amixer -q set Master 1%+")
+        , ((0, xF86XK_AudioMute), spawn "pactl set-sink-mute 0 toggle")
+        , ((0, xF86XK_AudioLowerVolume), spawn "pactl set-sink-volume 0 -1%")
+        , ((0, xF86XK_AudioRaiseVolume), spawn "pactl set-sink-volume 0 +1%")
         , ((mod4Mask, xK_F9), spawn "light -A 10")
         , ((mod4Mask, xK_F8), spawn "light -U 10")
-        , ((mod4Mask, xK_F5), spawn "amixer -q set Master toggle")
-        , ((mod4Mask, xK_F6), spawn "amixer -q set Master 1%-")
-        , ((mod4Mask, xK_F7), spawn "amixer -q set Master 1%+")
+--        , ((mod4Mask, xK_F5), spawn "amixer -q set Master toggle")
+--        , ((mod4Mask, xK_F6), spawn "amixer -q set Master 1%-")
+--        , ((mod4Mask, xK_F7), spawn "amixer -q set Master 1%+")
+        , ((mod4Mask, xK_F5), spawn "pactl set-sink-mute 0 toggle")
+        , ((mod4Mask, xK_F6), spawn "pactl set-sink-volume 0 -1%")
+        , ((mod4Mask, xK_F7), spawn "pactl set-sink-volume 0 +1%")
         , ((mod4Mask, xK_p), spawn "mpc toggle")
         , ((mod4Mask, xK_i), spawn "mpc volume -5")
         , ((mod4Mask, xK_o), spawn "mpc volume +5")
@@ -46,7 +53,6 @@ main = do
         , ((mod4Mask, xK_n), spawn "redshift")
         , ((mod4Mask .|. shiftMask, xK_n), spawn "killall redshift")
         , ((mod4Mask, xK_b), sendMessage ToggleStruts)
-        --, ((mod4Mask .|. shiftMask, xK_u), spawn "beesu ip link set wlp3s0 up")
-        --, ((mod4Mask .|. shiftMask, xK_d), spawn "beesu ip link set wlp3s0 down")
-        , ((mod4Mask .|. shiftMask, xK_m), spawn "xrandr --output VGA1 --mode 1920x1080 --pos 0x0 --rotate normal --output eDP1 --mode 1366x768 --pos 1920x580 --rotate normal")
+--        , ((mod4Mask .|. shiftMask, xK_m), spawn "xrandr --output VGA1 --mode 1920x1080 --pos 0x0 --rotate normal --output eDP1 --mode 1366x768 --pos 1920x580 --rotate normal")
+        , ((mod4Mask .|. shiftMask, xK_m), spawn "xrandr --output VGA1 --mode 1360x768 --pos 0x0 --rotate normal --right-of eDP1 --output eDP1 --mode 1366x768 --pos 1366x420 --rotate normal")
         ]
