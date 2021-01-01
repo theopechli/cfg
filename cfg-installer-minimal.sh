924dad482d6ab31ec0751971b89379bd6ddde7ff
Theofilos Pechlivanis <49034471+theopechli@users.noreply.github.com>
2021-01-01 12:39:57 +0200

    Add a minimal config installer

---
+++ cfg-installer-minimal.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo pacman -Syu
+sudo pacman -S --needed $(cat packages-minimal)
+
+cp -r .bash_profile .bashrc .config/ .xinitrc .xserverrc .xprofile dwm st ~/
+sudo cp -r xorg.conf.d /etc/X11/
+
+sudo systemctl set-default multi-user.target
