20b3c1c30f394a47030c59037173d1b080ff7ede
Theofilos Pechlivanis <49034471+theopechli@users.noreply.github.com>
2021-02-09 19:45:49 +0200

    Update emacs config

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,28 +1,12 @@
-(eval-and-compile
-  (package-initialize)
-  (unless (package-installed-p 'leaf)
-    (package-refresh-contents)
-    (package-install 'leaf))
-
-  (leaf hydra :ensure t)
-  (leaf avy :ensure t)
-  (leaf ivy :ensure t)
-  (leaf org :ensure t)
-  (leaf counsel :ensure t)
-  (leaf flymake :ensure t)
-  (leaf tramp :ensure t)
-  (leaf which-key :ensure t))
-
 (setq inhibit-startup-message t)
 
-(scroll-bar-mode -1)        ; Disable visible scrollbar
-(tool-bar-mode -1)          ; Disable the toolbar
-(tooltip-mode -1)           ; Disable tooltips
-(set-fringe-mode 10)        ; Give some breathing room
+(scroll-bar-mode -1)
+(tool-bar-mode -1)
+(tooltip-mode -1)
+(set-fringe-mode 10)
 
-(menu-bar-mode -1)            ; Disable the menu bar
+(menu-bar-mode -1)
 
-;; Set up the visible bell
 (setq visible-bell t)
 
 (column-number-mode)
@@ -30,101 +14,62 @@
 
 (show-paren-mode t)
 
-(set-face-attribute 'default nil
-                    :family "Noto Sans Mono"
-                    :height 100
-                    :weight 'normal
-                    :width 'normal)
-
-(avy-setup-default)
-(global-set-key (kbd "C-c C-j") 'avy-resume)
-(which-key-mode 1)
-(counsel-mode 1)
-(ivy-mode 1)
-(setq ivy-use-virtual-buffers t)
-(setq enable-recursive-minibuffers t)
-;; enable this if you want `swiper' to use it
-;; (setq search-default-mode #'char-fold-to-regexp)
-(global-set-key "\C-s" 'swiper)
-(global-set-key (kbd "C-c C-r") 'ivy-resume)
-(global-set-key (kbd "<f6>") 'ivy-resume)
-(global-set-key (kbd "M-x") 'counsel-M-x)
-(global-set-key (kbd "C-x C-f") 'counsel-find-file)
-(global-set-key (kbd "<f1> f") 'counsel-describe-function)
-(global-set-key (kbd "<f1> v") 'counsel-describe-variable)
-(global-set-key (kbd "<f1> o") 'counsel-describe-symbol)
-(global-set-key (kbd "<f1> l") 'counsel-find-library)
-(global-set-key (kbd "<f2> i") 'counsel-info-lookup-symbol)
-(global-set-key (kbd "<f2> u") 'counsel-unicode-char)
-(global-set-key (kbd "C-c g") 'counsel-git)
-(global-set-key (kbd "C-c j") 'counsel-git-grep)
-(global-set-key (kbd "C-c k") 'counsel-ag)
-(global-set-key (kbd "C-x l") 'counsel-locate)
-(global-set-key (kbd "C-S-o") 'counsel-rhythmbox)
-(global-set-key (kbd "C-c e") 'eshell)
-(global-set-key (kbd "C-c a") 'ansi-term)
-(global-set-key (kbd "C-c t") 'term)
-(define-key minibuffer-local-map (kbd "C-r") 'counsel-minibuffer-history)
-
-(defun html-template-vanilla (file)
-  (interactive "sEnter file path: ")
-  (append-to-file "<!DOCTYPE html>
-<html lang=\"el-GR\">
-<head>
-  <title></title>
-  <meta charset=\"utf-8\">
-  <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">
-</head>
-<body>
-
-</body>
-</html>
-" nil (concat file ".html")))
-
-(defun escape-quotes (@begin @end)
-  "Replace 「\"」 by 「\\\"」 in current line or text selection.
-See also: `xah-unescape-quotes'
-
-URL `http://ergoemacs.org/emacs/elisp_escape_quotes.html'
-Version 2017-01-11"
-  (interactive
-   (if (use-region-p)
-       (list (region-beginning) (region-end))
-     (list (line-beginning-position) (line-end-position))))
-  (save-excursion
-    (save-restriction
-      (narrow-to-region @begin @end)
-      (goto-char (point-min))
-      (while (search-forward "\"" nil t)
-        (replace-match "\\\"" "FIXEDCASE" "LITERAL")))))
-
-(defun unescape-quotes (@begin @end)
-  "Replace  「\\\"」 by 「\"」 in current line or text selection.
-See also: `xah-escape-quotes'
-
-URL `http://ergoemacs.org/emacs/elisp_escape_quotes.html'
-Version 2017-01-11"
-  (interactive
-   (if (use-region-p)
-       (list (region-beginning) (region-end))
-     (list (line-beginning-position) (line-end-position))))
-  (save-excursion
-    (save-restriction
-      (narrow-to-region @begin @end)
-      (goto-char (point-min))
-      (while (search-forward "\\\"" nil t)
-        (replace-match "\"" "FIXEDCASE" "LITERAL")))))
+(set-face-attribute 'default nil :font "Fira Code Retina" :height 100)
 
 (load-theme 'wombat)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages '(which-key leaf hydra counsel avy)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+
+(defalias 'yes-or-no-p 'y-or-n-p)
+
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+(straight-use-package 'use-package)
+(setq straight-use-package-by-default t)
+
+(use-package diminish)
+
+(use-package ivy
+  :init
+  (ivy-mode 1)
+  :diminish ivy-mode
+  :bind (("C-s" . swiper))
+  :config
+  (setq ivy-use-virtual-buffers t)
+  (setq ivy-count-format "(%d/%d) "))
+
+(use-package counsel
+  :init
+  (counsel-mode 1)
+  :diminish counsel-mode
+  :bind (("M-x" . counsel-M-x)
+	 ("C-x b" . counsel-ibuffer)))
+
+(use-package which-key
+  :init
+  (which-key-mode)
+  :diminish which-key-mode
+  :config
+  (setq which-key-idle-delay 1))
+
+(use-package projectile
+  :init
+  (when (file-directory-p "~/Projects/Git/")
+    (setq projectile-project-search-path '("~/Projects/Git")))
+  (setq projectile-switch-project-action #'projectile-dired)
+  :diminish projectile-mode
+  :config (projectile-mode)
+  :custom ((projectile-completion-system 'ivy))
+  :bind-keymap
+  ("C-c p" . projectile-command-map))
+
+(use-package magit)
