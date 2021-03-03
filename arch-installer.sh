3673d6f9d4845736f8ee5be64f9f04fb4c450fc0
Theofilos Pechlivanis <49034471+theopechli@users.noreply.github.com>
2021-03-03 16:48:55 +0200

    Update arch-instller.sh

--- arch-installer.sh
+++ arch-installer.sh
@@ -1,22 +1,33 @@
 #!/bin/bash
 
-set -uo pipefail
-trap 's=$?; echo "$0: Error on line "$LINENO": $BASH_COMMAND"; exit $s' ERR
 
-MIRRORLIST_URL_1="https://www.archlinux.org/mirrorlist/?country=GR&protocol=https&use_mirror_status=on"
-MIRRORLIST_URL_2="https://www.archlinux.org/mirrorlist/?country=DE&country=SE&protocol=https&use_mirror_status=on"
+## Select the mirrors
 
-pacman -Sy --noconfirm pacman-contrib
+dialog --yesno "Do you want to rank the mirrors?" 0 0
+response=$?
+if [ $response -eq 0 ]
+then
+    MIRRORLIST_URL_1="https://archlinux.org/mirrorlist/?country=GR&protocol=https&use_mirror_status=on"
+    MIRRORLIST_URL_2="https://archlinux.org/mirrorlist/?country=DE&country=SE&protocol=https&use_mirror_status=on"
 
-echo "Updating mirror list"
-curl -s "$MIRRORLIST_URL_1" | \
-    sed -e 's/^#Server/Server/' -e '/^#/d' | \
-    rankmirrors -n 5 - > /etc/pacman.d/mirrorlist
-curl -s "$MIRRORLIST_URL_2" | \
-    sed -e 's/^#Server/Server/' -e '/^#/d' | \
-    rankmirrors -n 5 - >> /etc/pacman.d/mirrorlist
+    pacman -Sy --noconfirm --needed pacman-contrib dialog
 
-### Get infomation from user ###
+    echo "Backing up the mirror list"
+    cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
+
+    echo "Updating the mirror list"
+    curl -s "$MIRRORLIST_URL_1" | \
+        sed -e 's/^#Server/Server/' -e '/^#/d' | \
+        rankmirrors -n 5 - > /etc/pacman.d/mirrorlist
+    curl -s "$MIRRORLIST_URL_2" | \
+        sed -e 's/^#Server/Server/' -e '/^#/d' | \
+        rankmirrors -n 5 - >> /etc/pacman.d/mirrorlist
+
+    [ -s /etc/pacman.d/mirrorlist ] || cp /etc/pacman.d/mirrorlist.backup /etc/pacman.d/mirrorlist
+fi
+
+## Get basic information
+
 region=$(dialog --stdout --inputbox "Enter region, e.g. 'Europe'" 0 0) || exit 1
 clear
 : ${region:?"region cannot be empty"}
@@ -44,13 +55,14 @@ devicelist=$(lsblk -dplnx size -o name,size | grep -Ev "boot|rpmb|loop" | tac)
 device=$(dialog --stdout --menu "Select installation disk" 0 0 0 ${devicelist}) || exit 1
 clear
 
-### Set up logging ###
-exec 1> >(tee "stdout.log")
-exec 2> >(tee "stderr.log")
+
+## Update the system clock
 
 timedatectl set-ntp true
 
-### Setup the disk and partitions ###
+
+## Partition the disk
+
 sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk "${device}"
   g # create a new empty GPT partition table
   n # add a new partition
@@ -95,10 +107,17 @@ part_root="$(ls ${device}* | grep -E "^${device}p?2$")"
 part_swap="$(ls ${device}* | grep -E "^${device}p?3$")"
 part_home="$(ls ${device}* | grep -E "^${device}p?4$")"
 
+
+## Format the partitions
+
 mkfs.fat -F32 "${part_efi}"
 mkfs.ext4 "${part_root}"
 mkfs.ext4 "${part_home}"
 mkswap "${part_swap}"
+
+
+## Mount the file systems
+
 swapon "${part_swap}"
 mount "${part_root}" /mnt
 mkdir /mnt/efi
@@ -106,31 +125,46 @@ mount "${part_efi}" /mnt/efi
 mkdir /mnt/home
 mount "${part_home}" /mnt/home
 
-### Install and configure the base system ###
+
+## Install essential packages
+
 pacstrap /mnt base base-devel linux linux-firmware grub efibootmgr intel-ucode git dhcpcd networkmanager neovim
+
+
+## Configure the system
+
+# Fstab
 genfstab -U /mnt >> /mnt/etc/fstab
 
+# Time zone
 arch-chroot /mnt ln -sf /usr/share/zoneinfo/"${region}"/"${city}" /etc/localtime
 arch-chroot /mnt hwclock --systohc
 
+# Localization
 arch-chroot /mnt sed -i 's/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/g' /etc/locale.gen
 arch-chroot /mnt sed -i 's/#el_GR.UTF-8 UTF-8/el_GR.UTF-8 UTF-8/g' /etc/locale.gen
 arch-chroot /mnt locale-gen
 echo "LANG=en_US.UTF-8" > /mnt/etc/locale.conf
 
+# Network configuration
 echo "${hostname}" > /mnt/etc/hostname
 echo -e "127.0.0.1       localhost
 ::1             localhost
-127.0.1.1       ${hostname}.localdomain	${hostname}
-" > /mnt/etc/hosts
+127.0.1.1       ${hostname}.localdomain	${hostname}" > /mnt/etc/hosts
 
+# Add user
 arch-chroot /mnt useradd -m -G wheel "$user"
 
+# Set root and user password
 echo "$user:$password" | chpasswd --root /mnt
 echo "root:$password" | chpasswd --root /mnt
 
+# Configure mkinitcpio
 arch-chroot /mnt grub-install --target=x86_64-efi --efi-directory=/efi --bootloader-id=GRUB --removable
 arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
+
+
+## Reboot
 
 arch-chroot /mnt git clone https://github.com/theopechli/cfg home/"${user}"/cfg
 arch-chroot /mnt systemctl enable NetworkManager.service
